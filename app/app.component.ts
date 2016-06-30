c4a1ce133cf7f60679510bef6fda5362c2970077
minkas-g-d <mina.dodunekova@gmail.com>
2016-06-30 11:29:04 +0300

    Add bootstrap specific stylesheet theme to the project and html for the navigation

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,7 +2,12 @@ import {Component} from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: `welcome`
+    template: `
+		<h1>Welcome to UP2 blog</h1>
+		<div class="container">
+			The content will change here.
+		</div>
+	`
 })
 export class AppComponent {
 }

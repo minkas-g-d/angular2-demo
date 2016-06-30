791b9188f2cd184e7b04755f19809f8ca8b52d45
minkas-g-d <mina.dodunekova@gmail.com>
2016-06-30 11:57:10 +0300

    add posts and display them in the root component (will move them to own component later on)

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,7 @@
 import {Component} from '@angular/core';
 import {NavbarComponent} from './navbar.component';
+import {PostsService} from './posts/posts.service';
+
 
 @Component({
     selector: 'my-app',
@@ -8,10 +10,28 @@ import {NavbarComponent} from './navbar.component';
 		<h1>{{pageTitle}}</h1>
 		<div class="container">
 			The content will change here.
+
+			<div class="row">
+				<div class="col-md-6">
+					<div class="all-posts" *ngFor="let post of posts">
+						<h2>{{post.title}}</h2>
+						<p>{{post.body}}</p>
+					</div>
+				</div>
+			</div>
 		</div>
 	`,
-	directives: [NavbarComponent]
+	directives: [NavbarComponent],
+	providers: [PostsService]
 })
 export class AppComponent {
 	pageTitle = 'Welcome to UP2 blog';
+	postService: PostsService;
+	posts: any[];
+
+	constructor() {
+		this.postService = new PostsService();
+
+		this.posts = this.postService.getPosts();
+	}
 }
---
+++ app/posts/posts.service.ts
@@ -0,0 +1,23 @@
+export class PostsService {
+	posts = [
+		{ 
+			title: 'Lorem ipsum dolor sit amet',
+			body: ' Nam sed posuere metus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce bibendum nunc ut ipsum pulvinar, in sagittis libero placerat. Mauris laoreet finibus libero eu blandit. Duis quis turpis accumsan, porttitor lorem et, vehicula velit. Mauris purus magna, sodales ut arcu at, tempus congue quam. Cras id metus et quam dapibus placerat ac a elit. '
+		},
+		{ 
+			title: 'Title of second blogpost',
+			body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec scelerisque sapien sit amet purus vehicula, vitae suscipit erat commodo. Phasellus sagittis consectetur rhoncus. Fusce ut lacus mi. Nulla rutrum sagittis maximus. Vestibulum efficitur eu est eget posuere. Suspendisse vitae lorem a dui luctus interdum.'
+		},
+		{ 
+			title: 'Title of third blogpost',
+			body: 'Praesent sit amet justo tortor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin vel pulvinar neque. '
+		},
+
+	];
+
+	getPosts() {
+		return this.posts;
+	}
+
+
+}

18c293af81fbc9111faa38c48ffd9340b2cd9aea
minkas-g-d <mina.dodunekova@gmail.com>
2016-06-30 11:37:46 +0300

    Add navbar component and display it

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,13 +1,17 @@
 import {Component} from '@angular/core';
+import {NavbarComponent} from './navbar.component';
 
 @Component({
     selector: 'my-app',
     template: `
-		<h1>Welcome to UP2 blog</h1>
+		<navbar></navbar>
+		<h1>{{pageTitle}}</h1>
 		<div class="container">
 			The content will change here.
 		</div>
-	`
+	`,
+	directives: [NavbarComponent]
 })
 export class AppComponent {
+	pageTitle = 'Welcome to UP2 blog';
 }
